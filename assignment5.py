ca138dbfde5e9b727eb41ba8831beaeb2a591d67
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-09 15:26:22 -0300

    some corrections to the assignment

--- assignment5.py
+++ assignment5.py
@@ -8,16 +8,16 @@ as well as a random number between 1 and 10.
 
 # 1) Import the random function and generate both a random number between 0 and 1
 # as well as a random number between 1 and 10.
-from random import random
+import random
 import datetime
 
-random_number_between_0_and_1 = random()
-random_number_between_1_and_10 = random() * 10 + 1
+random_number_between_0_and_1 = random.random()
+random_number_between_1_and_10 = random.randint(1, 10)
 print(random_number_between_0_and_1, random_number_between_1_and_10)
 
 # 2) Use the datetime library together with the random number to generate a random, unique value.
-random_ordinal = round(
-    random() * datetime.date.max.toordinal() + datetime.date.min.toordinal()
+random_ordinal = random.randint(
+    datetime.date.min.toordinal(), datetime.date.max.toordinal()
 )
 random_date = datetime.date.fromordinal(random_ordinal)
 print(random_date)
