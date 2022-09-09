ef1570f2a76e6410e5ce3fa27f637d2f2a709e17
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-09 17:25:44 -0300

    reading from files

--- files.py
+++ files.py
@@ -1,5 +1,6 @@
 """working with files"""
 
-demo_file = open("demo.txt", mode="w", encoding="utf-8")
-demo_file.write("Hello from Python!")
+demo_file = open("demo.txt", mode="r", encoding="utf-8")
+file_content = demo_file.read()
 demo_file.close()
+print(file_content)
