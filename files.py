e5a2677e4dae4cd70cba10632abfbaf7b1781045
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-12 11:35:33 -0300

    reading multi-line content

--- files.py
+++ files.py
@@ -1,5 +1,7 @@
 """working with files"""
 
-demo_file = open("demo.txt", mode="a", encoding="utf-8")
-demo_file.write("Add this content!\n")
+demo_file = open("demo.txt", mode="r", encoding="utf-8")
+# demo_file.write("Add this content!\n")
+file_content = demo_file.read()
 demo_file.close()
+print(file_content)
