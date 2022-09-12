76f3fd171d455bcf902fd5a9d10c39fff6032c7e
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-12 11:43:31 -0300

    using with statement

--- files.py
+++ files.py
@@ -1,16 +1,8 @@
 """working with files"""
 
-demo_file = open("demo.txt", mode="r", encoding="utf-8")
-# demo_file.write("Add this content!\n")
-# file_content = demo_file.read()
-# file_lines = demo_file.readlines()
-line = demo_file.readline()
-while line:
-    print(line)
+with open("demo.txt", mode="r", encoding="utf-8") as demo_file:
     line = demo_file.readline()
-demo_file.close()
-# print(file_content)
-# print(file_lines)
-
-# for line in file_lines:
-#     print(line[:-1])
+    while line:
+        print(line)
+        line = demo_file.readline()
+    demo_file.close()
