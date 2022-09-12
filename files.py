ff3a11000162905911c9b33260446a63ab5ac690
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-12 11:37:09 -0300

    reading lines into list

--- files.py
+++ files.py
@@ -2,6 +2,8 @@
 
 demo_file = open("demo.txt", mode="r", encoding="utf-8")
 # demo_file.write("Add this content!\n")
-file_content = demo_file.read()
+# file_content = demo_file.read()
+file_lines = demo_file.readlines()
 demo_file.close()
-print(file_content)
+# print(file_content)
+print(file_lines)
