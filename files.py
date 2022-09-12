bf05a518b26f69f62aac134391d1473ce858b18d
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-12 11:40:22 -0300

    getting first line

--- files.py
+++ files.py
@@ -3,10 +3,12 @@
 demo_file = open("demo.txt", mode="r", encoding="utf-8")
 # demo_file.write("Add this content!\n")
 # file_content = demo_file.read()
-file_lines = demo_file.readlines()
+# file_lines = demo_file.readlines()
+first_line = demo_file.readline()
 demo_file.close()
 # print(file_content)
-print(file_lines)
+# print(file_lines)
+print(first_line)
 
-for line in file_lines:
-    print(line[:-1])
+# for line in file_lines:
+#     print(line[:-1])
