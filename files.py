a5425352033d6bd636c4858445537f29c9fa0f81
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-12 11:42:07 -0300

    looping through file lines

--- files.py
+++ files.py
@@ -4,11 +4,13 @@ demo_file = open("demo.txt", mode="r", encoding="utf-8")
 # demo_file.write("Add this content!\n")
 # file_content = demo_file.read()
 # file_lines = demo_file.readlines()
-first_line = demo_file.readline()
+line = demo_file.readline()
+while line:
+    print(line)
+    line = demo_file.readline()
 demo_file.close()
 # print(file_content)
 # print(file_lines)
-print(first_line)
 
 # for line in file_lines:
 #     print(line[:-1])
