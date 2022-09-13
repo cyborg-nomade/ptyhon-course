ff742c4e9943c405ca0962bc65e47958f9fbd1f1
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-13 12:33:15 -0300

    warking with special methods

--- oop/car.py
+++ oop/car.py
@@ -10,6 +10,10 @@ class Car:
         self.top_speed = start_top_speed
         self.warnings = []
 
+    def __repr__(self) -> str:
+        print("Printing...")
+        return f"Top Speed: {self.top_speed}, Warnings: {len(self.warnings)}"
+
     def drive(self):
         """drives a car"""
         print(f"I am driving below {self.top_speed}")
@@ -19,6 +23,8 @@ car1 = Car()
 car1.drive()
 
 car1.warnings.append("New warning")
+print(car1.__dict__)
+print(car1)
 
 car2 = Car(300)
 car2.drive()
