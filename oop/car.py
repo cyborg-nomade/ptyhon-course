1c24ada0319a670ba7eb4a8365d29c64d078a33d
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-13 12:53:20 -0300

    encapsulating attributes

--- oop/car.py
+++ oop/car.py
@@ -8,11 +8,20 @@ class Car:
 
     def __init__(self, start_top_speed=100) -> None:
         self.top_speed = start_top_speed
-        self.warnings = []
+        self.__warnings = []
 
     def __repr__(self) -> str:
         print("Printing...")
-        return f"Top Speed: {self.top_speed}, Warnings: {len(self.warnings)}"
+        return f"Top Speed: {self.top_speed}, Warnings: {len(self.__warnings)}"
+
+    def add_warning(self, warning_text):
+        """adds a warning"""
+        if len(warning_text) > 0:
+            self.__warnings.append(warning_text)
+
+    def get_warnings(self):
+        """returns warnings"""
+        return self.__warnings
 
     def drive(self):
         """drives a car"""
@@ -22,11 +31,11 @@ class Car:
 car1 = Car()
 car1.drive()
 
-car1.warnings.append("New warning")
+car1.add_warning("New warning")
 print(car1.__dict__)
 print(car1)
 
 car2 = Car(300)
 car2.drive()
 
-print(car2.warnings)
+print(car1.get_warnings())
